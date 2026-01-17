8145f01d169130d9383235bde2a918df095b70bd
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-01-17 12:08:47 +0800

    feat: setup react frontend

---
+++ frontend/prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  semi: true,
+};
