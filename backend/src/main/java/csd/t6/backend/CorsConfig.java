84bcd4761a7e3a260e8d427098aa938000aaffb8
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-01-17 13:11:15 +0800

    chore: move cors setting to env var

--- backend/src/main/java/csd/t6/backend/CorsConfig.java
+++ backend/src/main/java/csd/t6/backend/CorsConfig.java
@@ -1,5 +1,6 @@
 package csd.t6.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+  @Value("${FRONTEND_URL}")
+  private String frontendUrl;
 
   @Bean
   WebMvcConfigurer corsConfigurer() {
@@ -14,9 +17,7 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-            // Remember to update origins here!
-            // Allowing credentials prevents us from just allowing all
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(frontendUrl)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
