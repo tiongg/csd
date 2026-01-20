4c075f47688a2f408d92721187db7946dd2a7bde
tiongg <tantiongguan123@gmail.com>
2026-01-20 21:21:28 +0800

    fix: hook BE to push changes to FE schema instead

--- frontend/dev-start.sh
+++ frontend/dev-start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-cp -rfu /cache/node_modules/. /app/node_modules/;
-
-# Tweak delay here as needed
-npx nodemon --watch /backend -e java --delay 5 --exec 'npm run generate-api' &
+npm i;
 
 npm run dev;
