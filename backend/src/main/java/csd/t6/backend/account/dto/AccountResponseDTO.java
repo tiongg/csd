ef885960f41dc42cc473a6ce7d4f9ce4a4c61566
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-01-25 22:46:43 +0800

    chore: return realname in account dto

--- backend/src/main/java/csd/t6/backend/account/dto/AccountResponseDTO.java
+++ backend/src/main/java/csd/t6/backend/account/dto/AccountResponseDTO.java
@@ -7,11 +7,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(name = "Account")
-public record AccountResponseDTO(
-    @NotNull UUID id,
-    @NotNull String email,
-    @NotNull String username) {
+public record AccountResponseDTO(@NotNull UUID id, @NotNull String email, @NotNull String username,
+    @NotNull String realName) {
   public AccountResponseDTO(AccountRecord account) {
-    this(account.getId(), account.getEmail(), account.getUsername());
+    this(account.getId(), account.getEmail(), account.getUsername(), account.getRealName());
   }
 }
