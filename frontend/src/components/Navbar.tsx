5fb9340fdef07b23f389c02531bc12c40461baed
lineonthepaper <lineonrl@gmail.com>
2026-02-04 00:10:28 +0800

    feat: hide navbar role selection for learners

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -23,64 +23,8 @@ export default function Navbar() {
         to="/"
       />
 
-      {user ? (
-        <DropdownMenu onOpenChange={setNavClicked}>
-          <DropdownMenuTrigger asChild>
-            <div className="flex cursor-pointer items-center justify-between rounded bg-white border py-1 px-4 h-[30px] w-40">
-              {
-                user.role !== "LEARNER" ? (
-                  <span>
-                    {dir.charAt(0).toUpperCase() + dir.slice(1).toLowerCase()}
-                  </span>
-                ) : (
-                  <Link to="/learner/dashboard" className="w-full text-sky-900">Learner</Link>
-                )
-              }
-              {
-                user.role !== "LEARNER" &&
-                  (
-                    navClicked ? (
-                      <div className='inline-block w-0 h-0 border-l-8 border-r-8 border-l-transparent border-r-transparent border-b-5 border-b-slate-400 ml-8'>
-                        <span className='sr-only'>Dropdown</span>
-                      </div>
-                    ) : (
-                      <div className='inline-block w-0 h-0 border-l-8 border-r-8 border-l-transparent border-r-transparent border-t-5 border-t-slate-400 ml-8'>
-                        <span className='sr-only'>Dropdown</span>
-                      </div>
-                    )
-                  ) 
-              }
-
-            </div>
-          </DropdownMenuTrigger>
-          {
-            user.role !== "LEARNER" && (
-              <DropdownMenuContent>
-                {
-                  user.role === "ADMIN" && (
-                    <DropdownMenuItem asChild>
-                      <Link to="/admin/dashboard" className="w-full text-sky-900">
-                        Admin
-                      </Link>
-                    </DropdownMenuItem>
-                  )
-                }
-                <DropdownMenuItem asChild>
-                  <Link to="/contributor/dashboard" className="w-full text-sky-900">
-                    Contributor
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <Link to="/learner/dashboard" className="w-full text-sky-900">
-                    Learner
-                  </Link>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            )
-          }
-        </DropdownMenu>
-      ) : (
-        <div>
+      {!user ? (
+          <div>
           <Button className='rounded-full mx-2' variant="outline">
             <Link to="/login">Log In</Link>
           </Button>
@@ -88,6 +32,49 @@ export default function Navbar() {
             <Link to="/register">Sign Up</Link>
           </Button>
         </div>
+        ) : user.role !== "LEARNER" && (
+        <DropdownMenu onOpenChange={setNavClicked}>
+          <DropdownMenuTrigger asChild>
+            <div className="flex cursor-pointer items-center justify-between rounded bg-white border py-1 px-4 h-[30px] w-40">
+              <span>
+                {dir.charAt(0).toUpperCase() + dir.slice(1).toLowerCase()}
+              </span>
+              {
+                navClicked ? (
+                  <div className='inline-block w-0 h-0 border-l-8 border-r-8 border-l-transparent border-r-transparent border-b-5 border-b-slate-400 ml-8'>
+                    <span className='sr-only'>Dropdown</span>
+                  </div>
+                ) : (
+                  <div className='inline-block w-0 h-0 border-l-8 border-r-8 border-l-transparent border-r-transparent border-t-5 border-t-slate-400 ml-8'>
+                    <span className='sr-only'>Dropdown</span>
+                  </div>
+                )
+              }
+
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {
+              user.role === "ADMIN" && (
+                <DropdownMenuItem asChild>
+                  <Link to="/admin/dashboard" className="w-full text-sky-900">
+                    Admin
+                  </Link>
+                </DropdownMenuItem>
+              )
+            }
+            <DropdownMenuItem asChild>
+              <Link to="/contributor/dashboard" className="w-full text-sky-900">
+                Contributor
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link to="/learner/dashboard" className="w-full text-sky-900">
+                Learner
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
     </header>
   );
