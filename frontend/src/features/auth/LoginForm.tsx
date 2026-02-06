b018f72c92f4ff85203f9cef27d003a353ffeaa0
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-02-06 23:34:19 +0800

    chore: change log in to login

--- frontend/src/features/auth/LoginForm.tsx
+++ frontend/src/features/auth/LoginForm.tsx
@@ -1,20 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Field, FieldError, FieldGroup } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { Heading1 } from '@/components/ui/typography';
+import { useAuth } from '@/context/AuthContext';
+import { constructAuthUrl } from '@/lib/auth-urls';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Link, useNavigate } from '@tanstack/react-router';
 import { Controller, useForm } from 'react-hook-form';
-import z from 'zod';
 import { FcGoogle } from 'react-icons/fc';
-import { Heading1 } from '@/components/ui/typography';
-import { Button } from '@/components/ui/button';
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from '@/components/ui/field';
-import { Input } from '@/components/ui/input';
-import { Separator } from '@/components/ui/separator';
-import { useAuth } from '@/context/AuthContext';
-import { constructAuthUrl } from '@/lib/auth-urls';
+import z from 'zod';
 
 const loginSchema = z.object({
   usernameOrEmail: z.string().min(3, 'Username must be at least 3 characters'),
@@ -55,19 +50,16 @@ export default function LoginForm() {
   }
 
   return (
-
-    <div className='h-full w-full flex flex-row-reverse'>
-      <div className='h-full w-1/2 bg-slate-200'>
-        {/* image here */}
-      </div>
-      <div className='h-full w-1/2 flex justify-center'>
-        <div className='p-18 w-8/10 min-w-[500px] flex flex-col justify-between'>
+    <div className="flex h-full w-full flex-row-reverse">
+      <div className="h-full w-1/2 bg-slate-200">{/* image here */}</div>
+      <div className="flex h-full w-1/2 justify-center">
+        <div className="flex w-8/10 min-w-[500px] flex-col justify-between p-18">
           <div>
-            <div className='flex justify-center'>
-              <Heading1>Log in</Heading1>
+            <div className="flex justify-center">
+              <Heading1>Login</Heading1>
             </div>
 
-            <form onSubmit={handleSubmit(onSubmit)} className="py-8 space-y-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 py-8">
               <FieldGroup>
                 <Controller
                   control={control}
@@ -79,8 +71,8 @@ export default function LoginForm() {
                         id="email-or-username"
                         aria-invalid={fieldState.invalid}
                         placeholder="Email or Username"
-                        className='text-slate-700 h-12'
-                        aria-label='Email or Username'
+                        className="h-12 text-slate-700"
+                        aria-label="Email or Username"
                       />
                       {fieldState.invalid && (
                         <FieldError errors={[fieldState.error]} />
@@ -102,8 +94,8 @@ export default function LoginForm() {
                         type="password"
                         aria-invalid={fieldState.invalid}
                         placeholder="Password"
-                        className='text-slate-700 h-12'
-                        aria-label='Password'
+                        className="h-12 text-slate-700"
+                        aria-label="Password"
                       />
                       {fieldState.invalid && (
                         <FieldError errors={[fieldState.error]} />
@@ -114,36 +106,50 @@ export default function LoginForm() {
               </FieldGroup>
 
               {errors.root && (
-                <div className="text-destructive text-sm">{errors.root.message}</div>
+                <div className="text-destructive text-sm">
+                  {errors.root.message}
+                </div>
               )}
 
-              <Button type="submit" className="w-full my-4" disabled={isSubmitting}>
+              <Button
+                type="submit"
+                className="my-4 w-full"
+                disabled={isSubmitting}
+              >
                 {isSubmitting ? 'Logging in...' : 'Login'}
               </Button>
             </form>
 
-            <div className='flex justify-center w-8/10 mx-auto'>
-              <div className='w-1/6'>
-                <Separator className='inline-block bg-slate-500' />
+            <div className="mx-auto flex w-8/10 justify-center">
+              <div className="w-1/6">
+                <Separator className="inline-block bg-slate-500" />
               </div>
-              <p className='font-subtitle inline-block w-2/3 px-4 text-center'>
-                Other log in options
+              <p className="font-subtitle inline-block w-2/3 px-4 text-center">
+                Other login options
               </p>
-              <div className='w-1/6'>
-                <Separator className='inline-block bg-slate-500' />
+              <div className="w-1/6">
+                <Separator className="inline-block bg-slate-500" />
               </div>
             </div>
-            <div className='flex justify-center'>
-              <Button variant="outline" className="size-16 m-4" size="icon-lg" asChild>
+            <div className="flex justify-center">
+              <Button
+                variant="outline"
+                className="m-4 size-16"
+                size="icon-lg"
+                asChild
+              >
                 <a href={constructAuthUrl('google')}>
-                  <FcGoogle className='size-8' />
+                  <FcGoogle className="size-8" />
                 </a>
               </Button>
             </div>
           </div>
 
-          <div className='font-[Noto_Sans] font-bold text-slate-700 text-center'>
-            Don't have an account? <Link to="/register" className='underline'>Sign up</Link>
+          <div className="text-center font-[Noto_Sans] font-bold text-slate-700">
+            Don't have an account?{' '}
+            <Link to="/register" className="underline">
+              Sign up
+            </Link>
           </div>
         </div>
       </div>
