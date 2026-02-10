8a96f7a201f90f1aeaecb64ecc13f9b90262cf9c
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-02-10 16:11:46 +0800

    refactor: cleanup doc on unmount

--- frontend/src/features/editor/CrepeEditor.tsx
+++ frontend/src/features/editor/CrepeEditor.tsx
@@ -1,22 +1,22 @@
 import { Crepe } from '@milkdown/crepe';
 import { collab, collabServiceCtx } from '@milkdown/plugin-collab';
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
+import { useEffect } from 'react';
 import { WebsocketProvider } from 'y-websocket';
 import * as Y from 'yjs';
 
 import '@milkdown/crepe/theme/common/style.css';
 import '@milkdown/crepe/theme/frame.css';
-import { useEffect } from 'react';
 
 function CrepeEditorInternal() {
-  const { get: editor } = useEditor((root) => {
+  const { get: getEditor } = useEditor((root) => {
     const editor = new Crepe({ root }).editor.use(collab);
 
     return editor;
   });
 
   useEffect(() => {
-    const editorInstance = editor();
+    const editorInstance = getEditor();
     if (!editorInstance) return;
 
     const doc = new Y.Doc();
@@ -40,7 +40,12 @@ function CrepeEditorInternal() {
         // connect yjs with milkdown
         .connect();
     });
-  }, [editor]);
+
+    return () => {
+      wsProvider.destroy();
+      doc.destroy();
+    };
+  }, [getEditor]);
 
   return <Milkdown />;
 }
