04c4027b2abfa4eddbe4201fc70aa2feed551d47
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-02-11 11:05:24 +0800

    refactor: use repository for pending contributor repository

--- backend/src/main/java/csd/t6/backend/contributor/PendingContributorRepository.java
+++ backend/src/main/java/csd/t6/backend/contributor/PendingContributorRepository.java
@@ -5,33 +5,20 @@ import static csd.t6.jooq.public_.tables.PendingContributors.PENDING_CONTRIBUTOR
 import java.util.UUID;
 
 import org.jooq.DSLContext;
-import org.jooq.Field;
 import org.springframework.stereotype.Repository;
 
+import csd.t6.backend.utils.BaseRepository;
 import csd.t6.jooq.public_.tables.records.PendingContributorsRecord;
 
 @Repository
-public class PendingContributorRepository {
-  private final DSLContext dsl;
-
+public class PendingContributorRepository extends BaseRepository<PendingContributorsRecord> {
   public PendingContributorRepository(DSLContext dsl) {
-    this.dsl = dsl;
+    super(dsl, PENDING_CONTRIBUTORS);
   }
 
-  public void insertPendingContributor(UUID learnerUUID) {
+  public PendingContributorsRecord insertPendingContributor(UUID learnerUUID) {
     PendingContributorsRecord record = dsl.newRecord(PENDING_CONTRIBUTORS);
     record.setLearnerId(learnerUUID);
-    record.store();
-  }
-
-  /**
-   * Generic method to check if a value exists in a specified field.
-   * 
-   * @param pendingContributors - the field to check
-   * @param learnerUUID         - the value to look for
-   * @return true if an entity with the value in the field exists, false otherwise
-   */
-  public <T> boolean exists(Field<T> field, T value) {
-    return dsl.fetchExists(dsl.selectOne().from(PENDING_CONTRIBUTORS).where(field.eq(value)));
+    return this.save(record);
   }
 }
