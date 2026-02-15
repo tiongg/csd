e430eebe59f1989da17bd4bc493544ea08262dad
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-02-15 23:28:33 +0800

    fix: center faq for admin

--- frontend/src/routes/_authenticated/admin/faq.tsx
+++ frontend/src/routes/_authenticated/admin/faq.tsx
@@ -41,29 +41,31 @@ const items = [
 
 function RouteComponent() {
   return (
-    <PageWithSideBar className="flex min-h-screen w-full">
-      <div className="m-auto w-full">
-        <Accordion
-          type="multiple"
-          className="mx-auto max-w-xl p-8"
-          defaultValue={['notifications']}
-        >
-          {items.map((item) => (
-            <AccordionItem key={item.value} value={item.value}>
-              <AccordionTrigger>{item.trigger}</AccordionTrigger>
-              <AccordionContent>{item.content}</AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-
-        <div className="flex flex-col space-y-4">
-          <p className="text-center">If there are any other questions</p>
-          <a
-            href="mailto:tg.tan.2024@computing.smu.edu.sg"
-            className="m-auto max-w-lg rounded-md bg-black px-4 py-2 text-sm font-medium text-white"
+    <PageWithSideBar className="h-[calc(100vh-4rem)] overflow-hidden">
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex w-full max-w-xl flex-col">
+          <Accordion
+            type="multiple"
+            className="p-8"
+            defaultValue={['notifications']}
           >
-            Contact Us
-          </a>
+            {items.map((item) => (
+              <AccordionItem key={item.value} value={item.value}>
+                <AccordionTrigger>{item.trigger}</AccordionTrigger>
+                <AccordionContent>{item.content}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+
+          <div className="flex flex-col space-y-4">
+            <p className="text-center">If there are any other questions</p>
+            <a
+              href="mailto:tg.tan.2024@computing.smu.edu.sg"
+              className="m-auto max-w-lg rounded-md bg-black px-4 py-2 text-sm font-medium text-white"
+            >
+              Contact Us
+            </a>
+          </div>
         </div>
       </div>
     </PageWithSideBar>
