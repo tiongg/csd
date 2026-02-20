506f5aae5b235dc129561574fa99a610e64ee4c1
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-02-20 22:48:38 +0800

    feat: display all pending applications

--- frontend/src/generated/api.d.ts
+++ frontend/src/generated/api.d.ts
@@ -304,6 +304,22 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/admin/contributor-applications": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: operations["getAllPendingApplications"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/": {
         parameters: {
             query?: never;
@@ -1183,6 +1199,26 @@ export interface operations {
                 };
                 content: {
                     "*/*": components["schemas"]["HttpErrorPayload"];
+                };
+            };
+        };
+    };
+    getAllPendingApplications: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["Account"][];
                 };
             };
         };
