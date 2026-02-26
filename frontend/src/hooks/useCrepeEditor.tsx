23579d26da34f0968fd1476f356588bc5b6aeddb
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-02-26 21:46:15 +0800

    feat: block image uploads of >5mb

--- frontend/src/hooks/useCrepeEditor.tsx
+++ frontend/src/hooks/useCrepeEditor.tsx
@@ -2,17 +2,23 @@ import { youtubeIframePlugin } from '@/features/editor/plugins/youtube.milkdown.
 import { Crepe, CrepeFeature } from '@milkdown/crepe';
 import { collab } from '@milkdown/plugin-collab';
 import { useEditor } from '@milkdown/react';
+import { toast } from 'sonner';
+
+const MAX_SIZE_MB = 5;
+const MAX_SIZE_BYTES = MAX_SIZE_MB * 1024 * 1024;
 
 export default function useCrepeEditor() {
   return useEditor((root) => {
     const crepe = new Crepe({
       root,
-      features: {
-        [CrepeFeature.ImageBlock]: true,
-      },
       featureConfigs: {
         [CrepeFeature.ImageBlock]: {
           onUpload: async (file: File) => {
+            if (file.size > MAX_SIZE_BYTES) {
+              toast.error(`File size exceeds ${MAX_SIZE_MB} MB limit`);
+              throw new Error(`File size exceeds ${MAX_SIZE_MB} MB limit`);
+            }
+
             return new Promise((resolve, reject) => {
               const reader = new FileReader();
               reader.onloadend = () => {
