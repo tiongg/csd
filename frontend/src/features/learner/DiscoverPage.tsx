4b0fd02e6fe4f4a52f53a908251bd3ee05ce02b9
lineonthepaper <lineonrl@gmail.com>
2026-02-26 19:19:45 +0800

    chore: autoplay video only when in view

--- frontend/src/features/learner/DiscoverPage.tsx
+++ frontend/src/features/learner/DiscoverPage.tsx
@@ -1,4 +1,5 @@
 import Autoplay from "embla-carousel-autoplay";
+import { useInView } from "react-intersection-observer";
 import type { PropsWithChildren } from 'react';
 import { Heading1, Heading3 } from '@/components/ui/typography';
 import {
@@ -23,7 +24,7 @@ function ReelOverlay({ course, description, children }: ReelOverlayProps) {
   return (
     <div className="h-[calc(100vh-16rem)] w-full relative border-2 border-slate-300">
       <div className="h-full w-full absolute z-1">
-        <div className="flex items-center py-4 flex-col">
+        <div className="flex items-center py-4 flex-col pb-4 bg-linear-to-b from-slate-500 to-transparent">
           <Button className="mb-4 cursor-pointer" size="lg">View Course</Button>
           <Heading3>{course}</Heading3>
           <p className="font-subtitle">{description}</p>
@@ -42,10 +43,14 @@ function ReelOverlay({ course, description, children }: ReelOverlayProps) {
   )
 }
 
-function ReelPlayer() {
+function ReelPlayer({src}: {src: string}) {
+  const { ref, inView } = useInView();
+
   return (
-    <div className="h-full w-full">
-      reel video here
+    <div className="h-full w-full flex justify-center items-center" ref={ref}>
+      <video loop autoPlay={inView} muted className="max-w-full max-h-full">
+        <source src={src} type="video/mp4"/>
+      </video>
     </div>
   )
 }
@@ -63,20 +68,20 @@ export default function DiscoverPage() {
       <Carousel className="w-full h-full"
         plugins={[
           Autoplay({
-            delay: 4000, stopOnInteraction: true
+            delay: 8000, stopOnInteraction: true
           })
         ]}
       >
         <CarouselContent>
           <CarouselItem>
-            <ReelOverlay course="skibidi" description="aaa">
-              <ReelPlayer />
+            <ReelOverlay course="skibidi" description="skibidi toilet">
+              <ReelPlayer src="/skibidi_toilet.mp4"/>
             </ReelOverlay>
           </CarouselItem>
 
           <CarouselItem>
-            <ReelOverlay course="skibidi" description="aaa">
-              <ReelPlayer />
+            <ReelOverlay course="skibidi" description="skibidi toilet">
+              <ReelPlayer src="/skibidi_toilet.mp4"/>
             </ReelOverlay>
           </CarouselItem>
         </CarouselContent>
