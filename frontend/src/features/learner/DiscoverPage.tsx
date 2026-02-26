775401fabdc31fda5f9dd84f120b847e32ae6fe5
lineonthepaper <lineonrl@gmail.com>
2026-02-26 19:19:45 +0800

    chore: increase button size

--- frontend/src/features/learner/DiscoverPage.tsx
+++ frontend/src/features/learner/DiscoverPage.tsx
@@ -24,7 +24,7 @@ function ReelOverlay({ course, description, children }: ReelOverlayProps) {
     <div className="h-[calc(100vh-16rem)] w-full relative border-2 border-slate-300">
       <div className="h-full w-full absolute z-1">
         <div className="flex items-center py-4 flex-col">
-          <Button className="mb-4 cursor-pointer">View Course</Button>
+          <Button className="mb-4 cursor-pointer" size="lg">View Course</Button>
           <Heading3>{course}</Heading3>
           <p className="font-subtitle">{description}</p>
         </div>
