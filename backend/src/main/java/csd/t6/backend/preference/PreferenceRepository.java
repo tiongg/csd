f5a70ee28e65dce6847aebfc9dade81dda61ce1d
Shutowith3e <joey.chik05@gmail.com>
2026-03-03 21:20:10 +0800

    feat: be connection to db

--- backend/src/main/java/csd/t6/backend/preference/PreferenceRepository.java
+++ backend/src/main/java/csd/t6/backend/preference/PreferenceRepository.java
@@ -1,6 +1,8 @@
 package csd.t6.backend.preference;
 
 import static csd.t6.jooq.accounts.tables.Preference.PREFERENCE;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,5 +21,21 @@ public class PreferenceRepository extends BaseRepository<PreferenceRecord> {
   public List<String> findTopics(UUID accountId) {
     return dsl.select(PREFERENCE.TOPIC).from(PREFERENCE).where(PREFERENCE.ACCOUNT_ID.eq(accountId))
         .fetch(PREFERENCE.TOPIC);
+  }
+
+  public List<PreferenceRecord> addPreference(UUID accountId, List<String> preference) {
+    List<PreferenceRecord> list = new ArrayList<>();
+    for (String pref : preference) {
+      PreferenceRecord rec = new PreferenceRecord(accountId, pref);
+      list.add(rec);
+    }
+    ;
+    this.dsl.batchInsert(list).execute();
+
+    // for (String pref : preference) {
+    // this.dsl.insertInto(PREFERENCE, PREFERENCE.ACCOUNT_ID,
+    // PREFERENCE.TOPIC).values(accountId, pref).execute();
+    // }
+    return list;
   }
 }
--- backend/src/main/java/csd/t6/backend/preference/PreferenceService.java
+++ backend/src/main/java/csd/t6/backend/preference/PreferenceService.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import csd.t6.backend.exceptions.BadRequestException;
+import csd.t6.jooq.accounts.tables.records.PreferenceRecord;
+
 @Service
 public class PreferenceService {
   private final PreferenceRepository preferenceRepository;
@@ -15,6 +18,13 @@ public class PreferenceService {
 
   public List<String> getTopics(UUID accountId) {
     return preferenceRepository.findTopics(accountId);
+  }
+
+  public List<PreferenceRecord> createNewPreference(UUID accountId, List<String> preferenceList) {
+    if (preferenceList.size() == 0) {
+      throw new BadRequestException("No preference selected");
+    }
+    return this.preferenceRepository.addPreference(accountId, preferenceList);
   }
 
 }
