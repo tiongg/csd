9dadc02f4ec2fc09e33978965c14d1d88515ac0a
lineonthepaper <lineonrl@gmail.com>
2026-03-08 22:27:30 +0800

    feat: add ReelsOverview to display reels if there are 1 or more

--- frontend/src/features/editor/course-overview/EditorCourseDisplay.tsx
+++ frontend/src/features/editor/course-overview/EditorCourseDisplay.tsx
@@ -4,6 +4,7 @@ import CourseEditorOverview from './CourseEditorOverview';
 import CourseVersions from './CourseVersions';
 import NoCourseSectionsYet from './NoCourseSectionsYet';
 import NoReelsYet from './NoReelsYet';
+import ReelsOverview from './ReelsOverview';
 
 type EditorCourseDisplayProps = {
   course: Course;
@@ -14,15 +15,15 @@ export default function EditorCourseDisplay({
 }: EditorCourseDisplayProps) {
   const { doc } = useContentEditor();
   const sectionCount = doc.getArray('root').length;
-  const reelsCount = 0;
+  const reelsCount = 1;
 
   return (
     <div className="flex h-full flex-col overflow-auto p-6">
       <div className="mx-auto w-full max-w-4xl space-y-2">
         <CourseEditorOverview course={course} />
-        <div className={sectionCount + reelsCount === 0 ? 'lg:grid lg:grid-cols-2 lg:gap-x-2' : ''}>
+        <div className={sectionCount + reelsCount === 0 ? 'lg:grid lg:grid-cols-2 lg:gap-x-2' : 'flex flex-col gap-y-2'}>
         {sectionCount === 0 && <NoCourseSectionsYet />}
-        {reelsCount === 0 && <NoReelsYet />}
+        {reelsCount === 0 ? <NoReelsYet /> : <ReelsOverview />}
         </div>
         <CourseVersions course={course} />
       </div>
---
+++ frontend/src/features/editor/course-overview/ReelsOverview.tsx
@@ -0,0 +1,48 @@
+import { FilePlay } from 'lucide-react';
+import { useBoolean } from 'usehooks-ts';
+import UploadReelDialog from './UploadReelDialog';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { CardWithPlusIcon } from '@/components/ui/custom-cards';
+
+export default function ReelsOverview() {
+    const {
+        value: isUploadReelDialogOpen,
+        setTrue: openUploadReelDialog,
+        setValue: setUploadReelDialogOpen,
+    } = useBoolean(false);
+    
+    return (
+        <div>
+            <Card>
+                <CardHeader>
+                    <div className='flex items-center gap-x-4'>
+                        <div className="bg-primary/10 flex size-12 items-center justify-center rounded-full">
+                            <FilePlay className="text-primary size-6" />
+                        </div>
+
+                        <div className="flex items-start justify-between">
+                            <div className="flex-1 space-y-2">
+                                <CardTitle className="text-2xl">Reels</CardTitle>
+                                <CardDescription className="text-base">
+                                    Reach out to more users by creating reels
+                                </CardDescription>
+                            </div>
+                        </div>
+                    </div>
+                </CardHeader>
+
+                <CardContent>
+                    <CardWithPlusIcon title="Add New Reel" onClick={openUploadReelDialog} />
+                    {/* reels go here */}
+                </CardContent>
+            </Card >
+            <UploadReelDialog isOpen={isUploadReelDialogOpen} setDialogOpen={setUploadReelDialogOpen} />
+        </div>
+    )
+}
