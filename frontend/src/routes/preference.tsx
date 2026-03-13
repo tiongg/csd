b662a1e88ce22023d1f19edbbeebec57e955e162
limweiyau <weiyau@live.com>
2026-03-13 17:29:44 +0800

    chore: commented out the welcome, name element

--- frontend/src/routes/preference.tsx
+++ frontend/src/routes/preference.tsx
@@ -102,7 +102,7 @@ function RouteComponent() {
     <div className="m-auto flex w-full flex-1 flex-col p-8">
       <div className="rounded-2xl border bg-white p-8">
         <div className="mb-6">
-          <p className="text-sm text-slate-500">Welcome {user?.realname},</p>
+          {/* <p className="text-sm text-slate-500">Welcome {user?.realname},</p> */}
           <p className="mt-2 text-2xl font-semibold text-slate-900">
             Getting to know you
           </p>
