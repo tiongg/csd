5fedde8de29f7c06276fc3978442d97efcb9d8b4
Tan Tiong Guan <tantiongguan123@gmail.com>
2026-04-03 21:54:30 +0800

    chore: cursor pointer course cards

--- frontend/src/features/contributor/CoursesList.tsx
+++ frontend/src/features/contributor/CoursesList.tsx
@@ -231,7 +231,7 @@ function CourseCard({ course, teamId }: CourseCardProps) {
       <div
         role="button"
         tabIndex={0}
-        className="h-full w-full text-left"
+        className="h-full w-full cursor-pointer text-left"
         onClick={handleCardClick}
         onKeyDown={handleKeyDown}
       >
@@ -349,7 +349,7 @@ function CreateCourseCard({ onInteract }: { onInteract: () => void }) {
   return (
     <button
       type="button"
-      className="h-full w-full text-left"
+      className="h-full w-full cursor-pointer text-left"
       onClick={onInteract}
     >
       <article className="group flex h-full min-h-40 flex-col justify-between rounded-xl border border-dashed border-slate-300 bg-white p-4 transition-all duration-150 hover:-translate-y-0.5 hover:border-sky-400 hover:bg-sky-50/30">
